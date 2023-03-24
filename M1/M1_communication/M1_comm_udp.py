ac584bb4c04be5d664a059478b718db86aea88b5
frantz <frantz.capiez@epi-rf.Fr>
2023-03-24 04:04:16 +0100

    Version initiale

---
+++ M1/M1_communication/M1_comm_udp.py
@@ -0,0 +1,13 @@
+import socket
+
+
+class M1_comm_udp:
+    def __init__(self,addr:str,port:int=12345):
+        self.bufferSize = 1024
+        self.m1AddressPort = (addr, port)
+        self.client = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+
+    def send_msg(self,bytesToSend:bytearray) -> bytes:
+        self.client.sendto(bytesToSend, self.m1AddressPort)
+        msgFromServer = self.client.recvfrom(self.bufferSize)
+        return msgFromServer[0]
