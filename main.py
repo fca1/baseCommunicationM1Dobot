1c7e31c99a968e6d0db4efafd75a99f901014f36
frantz <frantz.capiez@epi-rf.Fr>
2023-03-27 08:53:18 +0200

    Test poursuite avec homing

--- main.py
+++ main.py
@@ -30,25 +30,26 @@ protocol.queueCmdBase.setQueuedCmdForceStopExec()
 protocol.queueCmdBase.setQueuedCmdClear()
 
 # Faire un homing (Attention R ? )
-protocol.homeBase.setHome()
-while protocol.status == "ran":
-    time.sleep(2)
+if 0:
+    protocol.homeBase.setHome()
+    while protocol.status == "ran":
+        time.sleep(2)
 
 # Mettre par defaut une zone de travail  ( le tout en bout = 0)
 p0 = PositionArm(0, 0, 230)
 p1 = PositionArm(400, 0, 230)
-protocol.miscBase.setUserFrame(p0, p1)  # user tool
+protocol.miscBase.setUserFrame(p1, p0)  # user tool
 
 # Choisir une orientation et passer sur un point intermediaire apres avoir géré acceleration
-protocol.armOrientationBase.queued.setArmOrientation(False)
+protocol.armOrientationBase.queued.setArmOrientation(True)
 protocol.ptpBase.setPtpCoordinateParams(50,50,50,50)
 
-p1 = PositionArm(200,100,200)
-p2 = PositionArm(150,150,150)
+p1 = PositionArm(100,-100,4)
+p2 = PositionArm(100,100,4)
 
 while True:
-    protocol.armOrientationBase.setPTPCmd(p1,E_ptpMode.MOVL_XYZ)
-    protocol.armOrientationBase.setPTPCmd(p2,E_ptpMode.MOVL_XYZ)
+    protocol.armOrientationBase.setPTPCmd(p1,E_ptpMode.MOVJ_XYZ)
+    protocol.armOrientationBase.setPTPCmd(p2,E_ptpMode.MOVJ_XYZ)
 
 
 
